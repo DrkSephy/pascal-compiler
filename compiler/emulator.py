aa8657d1c4e6231e845442675b964c782594749f
David Leonard <sephirothcloud1025@yahoo.com>
2014-09-10 20:09:02 -0400

    Starting emulator.py

---
+++ compiler/emulator.py
@@ -0,0 +1,15 @@
+# -*- emulator.py -*-     
+# -*- MIT License (c) 2014 -*-
+# -*- drksephy.github.io -*-
+
+import sys
+
+def usage():
+    sys.stderr.write('Usage: Pascal filename\n')
+    sys.exit(1)
+
+
+
+
+
+
