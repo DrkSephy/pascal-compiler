f103c3dfc251e129737efc908a321d7702efeb17
David Leonard <sephirothcloud1025@yahoo.com>
2014-09-10 20:11:02 -0400

    Able to read files from command line using emulator.py

--- compiler/emulator.py
+++ compiler/emulator.py
@@ -8,7 +8,12 @@ def usage():
     sys.stderr.write('Usage: Pascal filename\n')
     sys.exit(1)
 
-
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+    filename = sys.argv[1]
+    text = open(filename).read()
+    print text
 
 
 
