e4fde130c257dda477e9e28479e795d7fd8a0423
David Leonard <sephirothcloud1025@yahoo.com>
2014-09-12 11:48:50 -0400

    scanner.py: Updating notes

--- compiler/scanner.py
+++ compiler/scanner.py
@@ -2,6 +2,13 @@
 # -*- MIT License (c) 2014 David Leonard -*-
 # -*- drksephy.github.io -*-
 
+#################
+#     TODO      #
+#################
+
+# - [done] Scan <target program>
+# - [    ] Tokenize keywords
+
 import sys
 
 def scan(source):
@@ -19,33 +26,37 @@ def scan(source):
     text = open(source, 'r').read().splitlines()
     for line in text:
         curr_col = 1
-        for char in line:             
+        for char in line: 
+                    
             # Treat all ascii chars <= 32 as spaces
             if to_ascii(char) == 32:
                 print "row: " + str(curr_row) + " , " + "col: " + str(curr_col) + " is: " + char
             # Check if char: ! " # $ % & ' ( ) * + , - . / : ; < = > ? @
+
             if ((to_ascii(char) > 32 and to_ascii(char) < 47) or (to_ascii(char) > 57 and to_ascii(char) < 65)):
                 print "row: " + str(curr_row) + " , " + "col: " + str(curr_col) + " is: " + char
+
             # Check if char: 0 1 2 3 4 5 6 7 8 9
             if to_ascii(char) > 47 and to_ascii(char) < 58:
                 print "row: " + str(curr_row) + " , " + "col: " + str(curr_col) + " is: " + char
             # Check if char is uppercase
+
             if to_ascii(char) > 64 and to_ascii(char) < 91:
                 print "row: " + str(curr_row) + " , " + "col: " + str(curr_col) + " is: " + char 
             # Check if char is lowercase
+
             if to_ascii(char) > 96 and to_ascii(char) < 123:
                 print "row: " + str(curr_row) + " , " + "col: " + str(curr_col) + " is: " + char
+
             # Check if char: [ \ ] ^ _ `
             if to_ascii(char) > 90 and to_ascii(char) < 97:
                 print "row: " + str(curr_row) + " , " + "col: " + str(curr_col) + " is: " + char
+
             # Check if char: { | } ~ DEL
             if to_ascii(char) > 122 and to_ascii(char) < 128:
                 print "row: " + str(curr_row) + " , " + "col: " + str(curr_col) + " is: " + char
             curr_col += 1
         curr_row += 1
-
-
-        
 
 
 def lookup(table, key):
@@ -83,6 +94,7 @@ OPERATORS = {
     '+'         : 'TK_PLUS',
     '-'         : 'TK_MINUS',
     '*'         : 'TK_MULT',
+    '/'         : 'TK_DIV_FLOAT',
     'DIV'       : 'TK_DIV',
     'MOD'       : 'TK_MOD',
     ':'         : 'TK_COLON',
