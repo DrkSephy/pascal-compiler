321bf8d200123423a791d24b7e3ca8d8ed59b46a
David Leonard <sephirothcloud1025@yahoo.com>
2014-09-13 21:01:53 -0400

    scanner.py: Intermediate work on tokenizing entire program

--- compiler/scanner.py
+++ compiler/scanner.py
@@ -147,10 +147,21 @@ class Scanner(object):
         if self.to_ascii(char) <= 32: 
             # If current token exists, we append it
             if self.curr_token:
-                self.tokens.append(self.curr_token)
-                self.curr_token = ''
-                self.curr_val = ''
-                return 
+                if self.to_upper(self.curr_val) in self.KEYWORDS:
+                    print self.curr_val
+                    self.curr_token = self.lookup(self.KEYWORDS, self.to_upper(self.curr_val))
+                    self.tokens.append(self.curr_token)
+                    self.curr_token = ''
+                    self.curr_val = ''
+                    return
+
+                if self.to_upper(self.curr_val) in self.OPERATORS:
+                    print self.curr_val
+                    self.curr_token = self.lookup(self.OPERATORS, self.to_upper(self.curr_val))
+                    self.tokens.append(self.curr_token)
+                    self.curr_token = ''
+                    self.curr_val = ''
+                    return 
 
         # Character is a semicolon
         if self.to_ascii(char) == 59:
@@ -189,14 +200,6 @@ class Scanner(object):
 
         # If none of the above cases are true
         self.curr_val += char
-
-        if self.to_upper(self.curr_val) in self.KEYWORDS: 
-            print self.to_upper(self.curr_val)
-            self.curr_token = self.lookup(self.KEYWORDS, self.to_upper(self.curr_val))
-
-        if self.to_upper(self.curr_val) in self.OPERATORS:
-            print self.curr_val
-            self.curr_token = self.lookup(self.OPERATORS, self.to_upper(self.curr_val))
 
         # string is not in either table
         if self.to_upper(self.curr_val) not in self.KEYWORDS:
