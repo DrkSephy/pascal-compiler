e12d2629a3300806898d2d1de593a4b4c83cf103
David Leonard <sephirothcloud1025@yahoo.com>
2014-09-25 00:05:08 -0400

    parser.py: Adding barebones parser

---
+++ compiler/parser.py
@@ -0,0 +1,12 @@
+# -*- parser.py -*-
+# -*- MIT License (c) 2014 David Leonard -*-
+# -*- drksephy.github.io -*-
+
+# * Pascal Parser
+# * Consumes tokens one at a time, builds parse tree
+
+
+class Parser(object):
+
+    def __init__(self, tokens):
+        pass
