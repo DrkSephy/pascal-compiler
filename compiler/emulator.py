3b6eea394b22d7ab9618086172cb45fe2b53bb1c
David Leonard <sephirothcloud1025@yahoo.com>
2014-11-02 20:16:14 -0500

    emulator.py: Updating initialization of simulator

--- compiler/emulator.py
+++ compiler/emulator.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     # Return the AST using tokens
     ast      = parser.parse()
     # Initialize the simulator
-    simulator = Simulator(ast)
+    simulator = Simulator(ast, [], [], 0)
     # Simulate the AST
     simulation = simulator.simulate(ast)
 
