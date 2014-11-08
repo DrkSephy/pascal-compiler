8a5724ac3f616b689cf9ab4fccbcf51e2de5f43e
David Leonard <sephirothcloud1025@yahoo.com>
2014-11-08 18:56:27 -0500

    simulator.py: Implemented or op code

--- compiler/simulator.py
+++ compiler/simulator.py
@@ -44,6 +44,8 @@ class Simulator(object):
                     self.pushi(node['value'])
                 else:
                     self.push(node['value'])
+            if node['instruction'] == 'or':
+                self.op_or()
             if node['instruction'] == 'mod':
                 self.mod()
             if node['instruction'] == 'div_float':
@@ -85,6 +87,13 @@ class Simulator(object):
     #--------------------------
     #         OP CODES
     #--------------------------
+
+    def op_or(self):
+        val = int(self.stack[1]) or int(self.stack[0])
+        self.stack.remove(self.stack[1])
+        self.stack.remove(self.stack[0])
+        self.push(val)
+        return
 
     def mod(self):
         val = int(self.stack[1]) % int(self.stack[0])
