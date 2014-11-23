c9068588c38e3b79b109e581b5472b8734af2c2f
David Leonard <sephirothcloud1025@yahoo.com>
2014-11-23 00:17:12 -0500

    examples/repeat.pas: Testing more complicated programs

--- examples/repeat.pas
+++ examples/repeat.pas
@@ -1,1 +1,1 @@
-PROGRAM var x : integer; BEGIN     x := 17 ;    repeat        x := x + 1 ;    until x > 100 ; END.
+PROGRAM var x , y : integer;var a , b , c : real;BEGIN     x := 17 ;    repeat        x := x + 1 ;    until x > 100 ; END.
