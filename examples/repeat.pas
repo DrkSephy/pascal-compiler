4c97d1199901f08e4c1aecb2ab29222eacc3a1b2
David Leonard <sephirothcloud1025@yahoo.com>
2014-11-25 11:21:35 -0500

    examples/repeat.pas: Updating repeat example with more complicated examples

--- examples/repeat.pas
+++ examples/repeat.pas
@@ -1,1 +1,1 @@
-PROGRAM var x , y , z : integer;var a , b , c : real;BEGIN     x := 0 ;    y := 0 ;    z := 0 ;    repeat        x := x + 1 ;        y := y + 1 ;        z := z + 2 ;    until x > 5 ; END.
+PROGRAM var x , y , z : integer;var a , b , c : real;BEGIN     x := 0 ;    y := 0 ;    z := 0 ;    repeat        x := x + 1 ;        y := y + 1 ;        z := z + 2 ;    until x > 5 ;     a := 8 * 2 ;    b := a * 2 ;    c := b / 7 ;END.
