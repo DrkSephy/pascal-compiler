19c95f7f34d92c49666ef508a2831fe90df19d53
David Leonard <sephirothcloud1025@yahoo.com>
2014-11-25 00:25:23 -0500

    examples/repeat.pas: Testing multiple statements within repeat statements

--- examples/repeat.pas
+++ examples/repeat.pas
@@ -1,1 +1,1 @@
-PROGRAM var x , y : integer;var a , b , c : real;BEGIN     x := 0 ;    repeat        x := x + 1 ;    until x > 5 ; END.
+PROGRAM var x , y , z : integer;var a , b , c : real;BEGIN     x := 0 ;    y := 0 ;    z := 0 ;    repeat        x := x + 1 ;        y := y + 1 ;        z := z + 2 ;    until x > 5 ; END.
