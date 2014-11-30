c2a68777025cf5f10be31526683bdba7f85374f9
David Leonard <sephirothcloud1025@yahoo.com>
2014-11-30 17:34:28 -0500

    parser.py: Removing simulator code

--- compiler/parser.py
+++ compiler/parser.py
@@ -242,7 +242,7 @@ class Parser(object):
             self.match('TK_THEN')
             self.statements()
         else: 
-            
+
         return 
 
     def repeat(self): 
@@ -466,51 +466,6 @@ class Parser(object):
             self.match('TK_CLOSE_PARENTHESIS')
             return
 
-    #--------------------------
-    #         SIMULATOR
-    #--------------------------
-    
-    def simulate(self, node):
-        print "instruction: " + node['instruction'] + ", value: " + node['value'] +  ", ip: " + str(self.ip)
-        if node['instruction'] == 'push':
-            if node['token'] == 'TK_IDENTIFIER':
-                self.pushi(node['value'])
-            else:
-                self.push(node['value'])
-        if node['instruction'] == 'or':
-            self.op_or()
-        if node['instruction'] == 'and':
-            self.op_and()
-        if node['instruction'] == 'xor':
-            self.op_xor()
-        if node['instruction'] == 'not':
-            self.op_not()
-        if node['instruction'] == 'mod':
-            self.mod()
-        if node['instruction'] == 'less':
-            self.op_less()
-        if node['instruction'] == 'greater':
-            self.op_greater()
-        if node['instruction'] == 'less_equals':
-            self.op_lesseq()
-        if node['instruction'] == 'greater_equals':
-            self.op_greatereq()
-        if node['instruction'] == 'not_equals':
-            self.op_noteq()
-        if node['instruction'] == 'equals':
-            self.op_equals()
-        if node['instruction'] == 'div_float':
-            self.div_float()
-        if node['instruction'] == 'add':
-            self.add()
-        if node['instruction'] == 'pop':
-            self.pop(node['value'])
-        if node['instruction'] == 'minus':
-            self.minus()
-        if node['instruction'] == 'mult':
-            self.mult()
-        print self.stack
-
     #----------------------------------------
     #        DECORATED GRAMMAR METHODS                
     #----------------------------------------
@@ -594,134 +549,6 @@ class Parser(object):
             del storage[:]
             iterator += 1
         return table
-
-    #--------------------------
-    #         OP CODES
-    #--------------------------
-
-    def op_lesseq(self):
-        val = int(self.stack[1]) <= int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return
-
-    def op_noteq(self):
-        val = int(self.stack[1]) != int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return
-
-    def op_equals(self):
-        val = int(self.stack[1]) == int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return
-
-    def op_greatereq(self):
-        val = int(self.stack[1]) >= int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return
-
-
-    def op_greater(self):
-        val = int(self.stack[1]) > int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return
-
-    def op_less(self):
-        val = int(self.stack[1]) < int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return 
-
-    def op_not(self):
-        val = not int(self.stack[0])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return 
-
-    def op_and(self):
-        val = int(self.stack[1]) and int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return
-
-    def op_xor(self):
-        val = int(self.stack[1]) ^ int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return
-
-    def op_or(self):
-        val = int(self.stack[1]) or int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return
-
-    def mod(self):
-        val = int(self.stack[1]) % int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return 
-
-    def div_float(self):
-        val = int(self.stack[1]) / int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return 
-
-    def pushi(self, value):
-        for var in self.symtable:
-            if var['NAME'] == value:
-                self.stack.insert(0, var['VALUE'])
-        return
-
-    def push(self, value):
-        self.stack.insert(0, value)
-        return
-
-    def pop(self, value):
-        val = self.stack[0]
-        self.stack.remove(self.stack[0])
-        for var in self.symtable:
-            if var['NAME'] == value:
-                var['VALUE'] = val
-        return
-
-    def mult(self):
-        val = int(self.stack[1]) * int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return
-
-    def add(self):
-        val = int(self.stack[1]) + int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return
-
-
-    def minus(self):
-        val = int(self.stack[1]) - int(self.stack[0])
-        self.stack.remove(self.stack[1])
-        self.stack.remove(self.stack[0])
-        self.push(val)
-        return
 
     #--------------------------
     #       HELPER METHODS
